66601339b1e333895b5e065f5e557a0a11d9e2d0
Henry Hammond <henryhhammond92@gmail.com>
2016-07-18 23:44:51 -0400

    Created MVP of analytics logging service

---
+++ events.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	PageEventSchemaVersion = "1"
+)
+
+type PageEvent struct {
+	SchemaVersion  string    `json:"schema_version"`
+	ScriptVersion  string    `json:"script_version"`
+	Time           time.Time `json:"datetime"`
+	Host           string    `json:"server"`
+	RemoteAddr     string    `json:"remote_addr"`
+	UserAgent      string    `json:"user_agent"`
+	RequestReferer string    `json:"referer"`
+	Title          string    `json:"title"`
+	PageReferrer   string    `json:"referer"`
+	URL            string    `json:"url"`
+	EventType      string    `json:"event_type"`
+	SessionToken   string    `json:"session_token"`
+	UserToken      string    `json:"user_token"`
+}
+
+func PageEventFromRequest(req *http.Request) *PageEvent {
+	params := req.URL.Query()
+	return &PageEvent{
+		SchemaVersion:  PageEventSchemaVersion,
+		Host:           req.Host,
+		RemoteAddr:     req.RemoteAddr,
+		UserAgent:      req.UserAgent(),
+		RequestReferer: req.Referer(),
+		Time:           time.Now(),
+
+		Title:         params.Get("title"),
+		PageReferrer:  params.Get("referer"),
+		URL:           params.Get("url"),
+		ScriptVersion: params.Get("version"),
+		EventType:     params.Get("event_type"),
+		SessionToken:  params.Get("session_token"),
+		UserToken:     params.Get("user_token"),
+	}
+}
+
+func LogPageEvent(req *http.Request) {
+	event := PageEventFromRequest(req)
+	json, err := json.Marshal(event)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to log json data")
+	} else {
+		fmt.Println(string(json))
+	}
+}
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+const pixelRaw = "R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+
+func makeRequestPixel() http.HandlerFunc {
+	pixel, err := base64.StdEncoding.DecodeString(pixelRaw)
+	if err != nil {
+		panic(err)
+	}
+
+	return func(out http.ResponseWriter, req *http.Request) {
+		go LogPageEvent(req)
+		out.Header().Set("Content-Type", "image/gif")
+		out.WriteHeader(http.StatusOK)
+		out.Write(pixel)
+	}
+}
+
+func init() {
+}
+
+func main() {
+	http.HandleFunc("/a.gif", makeRequestPixel())
+
+	fmt.Fprintf(os.Stderr, "Starting server.")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to start server")
+	}
+}
