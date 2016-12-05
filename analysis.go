1d43188b35463912a5b5cdf835ac3d183cba997f
Henry Hammond <henry.hammond@shopify.com>
2016-12-04 23:20:08 -0500

    add report functionality

---
+++ analysis.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	sqlite "github.com/mattn/go-sqlite3"
+	"net/url"
+)
+
+func sqlite3Hostname(s string) string {
+	u, err := url.Parse(s)
+	if err != nil {
+		return ""
+	}
+	return u.Host
+}
+
+func registerSQLiteExtensions(conn *sqlite.SQLiteConn) error {
+	if err := conn.RegisterFunc("hostname", sqlite3Hostname, true); err != nil {
+		return err
+	}
+
+	return nil
+}
